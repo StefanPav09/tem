4f3659c594df6891bdbd29c57a9a6b8cc0fed185
StefanPav09 <150288215+StefanPav09@users.noreply.github.com>
2023-11-14 20:15:51 +0200

    Update service.js

---
+++ tema2/server/service.js
@@ -0,0 +1,23 @@
+const {existsSync, readFileSync, writeFileSync} = require('fs');
+module.exports = function(path) {
+    const tasksByStatus = existsSync(path)
+        ? JSON.parse(readFileSync(path)) : {};
+    return {
+        getTasksByStatus() {
+            return tasksByStatus;
+        },
+        changeTaskStatus(task, oldStatus, newStatus) {
+            if (tasksByStatus[oldStatus] instanceof Array
+                && tasksByStatus[newStatus] instanceof Array) {
+                let index = tasksByStatus[oldStatus].findIndex(t => t === task);
+                if (index) {
+                    tasksByStatus[oldStatus].splice(index, 1);
+                    tasksByStatus[newStatus].push(task);
+                    writeFileSync(path, JSON.stringify(tasksByStatus));
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+};
